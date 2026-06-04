ef184bf63947d6779f6fbc6dbc240ae7efc1c561
eeabderrahim546-cloud <eeabderrahim546@gmail.com>
2026-06-04 22:33:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,24 @@
-const CACHE_NAME = 'elzer-v6';
+const CACHE_NAME = 'elzer-v7';
 const ASSETS = [
-  '/accueil.html',
-  '/admin-login.html', 
-  '/admin.html',
-  '/index.html',
-  '/manifest.json',
-  '/manifest-admin.json',
-  '/icon.svg'
+  '/elzer/accueil.html',
+  '/elzer/admin-login.html',
+  '/elzer/admin-install.html',
+  '/elzer/admin.html',
+  '/elzer/index.html',
+  '/elzer/manifest.json',
+  '/elzer/manifest-admin.json',
+  '/elzer/icon-192.png',
+  '/elzer/icon-512.png',
+  '/elzer/icon.svg'
 ];
 
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return Promise.allSettled(ASSETS.map(url => cache.add(url).catch(e => console.log('Cache skip:', url))));
+      return Promise.allSettled(ASSETS.map(url => 
+        cache.add(url).catch(e => console.log('Cache skip:', url))
+      ));
     })
   );
 });
@@ -21,7 +26,10 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => {
+        console.log('Suppression ancien cache:', k);
+        return caches.delete(k);
+      }))
     ).then(() => self.clients.claim())
   );
 });
